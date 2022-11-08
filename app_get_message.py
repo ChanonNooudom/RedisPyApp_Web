c1d9906b4872d5eaa86bf996760cba73cd0d9da1
chanon <chanon@planetcomm.com>
2022-11-08 10:34:01 +0700

    .

--- app_get_message.py
+++ app_get_message.py
@@ -1,12 +1,15 @@
 import redis
 import time
+import json
 
 #health check interval must be higher than get_message interval
 #if get_message interval = 1, health check interval must be > 1
 r = redis.Redis(host='localhost', port=6379, decode_responses=True, health_check_interval=2)
 
 m = r.get("variable_a")
-print(m)
+print (type(m))
+mm  = json.loads(m)
+print(mm['data'])
 
 q = r.pubsub()
 q.subscribe("channel_a")
@@ -14,7 +17,11 @@ while True:
     try:
         m = q.get_message()
         if m:
-            print(m)
+            if ("data" in m.keys()):
+                my_data = m["data"]
+                if type(my_data) is str :
+                    j = json.loads(my_data)
+                    print(j["message"])
         time.sleep(1)
     except:
         print("lost connection wtih redis server")
